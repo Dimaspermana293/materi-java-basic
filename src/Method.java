e6dd259c3bd03c290f3593b267f2d5a4b1f92f26
dimaspermana293 <dimaspermana293>
2023-03-07 21:48:02 +0700

    Add Method training

---
+++ src/Method.java
@@ -0,0 +1,12 @@
+public class Method {
+    public static void main(String[] args){
+        sayHelloWorld();
+    }
+
+    static void sayHelloWorld(){
+        System.out.println("Halo Guys");
+
+
+    }
+
+}
