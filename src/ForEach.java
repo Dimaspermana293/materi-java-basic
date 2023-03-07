c28465f0899f7551cc4463e91b4d5b1ef26794b2
dimaspermana293 <dimaspermana293>
2023-03-07 21:47:49 +0700

    Add ForEach training

---
+++ src/ForEach.java
@@ -0,0 +1,14 @@
+public class ForEach {
+    public static void main (String[] args){
+        String[] names = {
+                "Dimas", "Permana","Masuk","Yaa"
+        };
+
+        //mengambil data satu persatu
+        System.out.println("FOR EACH");
+        for (String value : names){
+            System.out.println(value);
+        }
+    }
+
+}
