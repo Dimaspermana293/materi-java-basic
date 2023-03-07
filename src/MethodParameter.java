c96434e6ee4283d1440a1422ed49690acf06f043
dimaspermana293 <dimaspermana293>
2023-03-07 21:48:47 +0700

    Add MethodParameter training

---
+++ src/MethodParameter.java
@@ -0,0 +1,9 @@
+public class MethodParameter {
+    public static void main(String[] args) {
+        sayPengunjung("Dimas","Permana");
+    }
+
+    static void sayPengunjung(String firstname, String lastname){
+        System.out.println("Selamat Datang di Kebun Binatang " + firstname + lastname);
+    }
+}
